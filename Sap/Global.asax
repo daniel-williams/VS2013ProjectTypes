7dcc18ee134a96dd9494a765ab8d5fd9b551076e
daniel-williams <mail@danielwilliams.us>
2015-06-21 17:13:29 -0700

    Sap Initial Commit

---
+++ Sap/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Sap.MvcApplication" Language="C#" %>
---
+++ Sap/Models/MeViewModels.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Sap.Models
+{
+    // Models returned by MeController actions.
+    public class GetViewModel
+    {
+        public string Hometown { get; set; }
+    }
+}
